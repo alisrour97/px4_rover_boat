1acca9d44cbecc9ada1109b1c305c500313009d0
Salvatore Marcellini <salvatore.marcellini@gmail.com>
2023-06-25 18:48:19 +0200

    simulate script fix

--- simulate.py
+++ simulate.py
@@ -10,6 +10,9 @@ parser = argparse.ArgumentParser(description='Multiple px4 trajectory simulation
 parser.add_argument('n_iter', type=int,
                     help='Number of simulations')
 
+parser.add_argument('traj_length', type=int,
+                    help='Trajectory duration in seconds')
+
 args = parser.parse_args()
 
 client = gazebo = subprocess.Popen("gnome-terminal -- ./../Micro-XRCE-DDS-Agent/build/MicroXRCEAgent udp4 -p 8888",
@@ -21,10 +24,10 @@ for i in range(args.n_iter) :
 
     gazebo = subprocess.Popen("gnome-terminal -- docker exec -it px4_ros2 bash -c 'cd PX4-Autopilot; HEADLESS=1 make px4_sitl gazebo-classic'",
                               stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
-    time.sleep(1)
+    time.sleep(10)
     traj = subprocess.Popen("gnome-terminal -- docker exec -it px4_ros2 bash -c 'cd sensitivity_experiment; source install/setup.bash; ros2 run trajectory_publisher trajectory_publisher'",
                             stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
 
-    time.sleep(2)
+    time.sleep(args.traj_length + 5)
     stop = subprocess.Popen("docker stop px4_ros2",stdout=subprocess.PIPE,universal_newlines=True, shell=True)
     time.sleep(2)
