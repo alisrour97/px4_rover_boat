310198af8312b9584fd0694a1d23038e8f28e1fa
Salvatore Marcellini <salvatore.marcellini@gmail.com>
2023-06-28 11:33:50 +0200

    simulation script

--- simulate.py
+++ simulate.py
@@ -20,16 +20,18 @@ args = parser.parse_args()
 client = gazebo = subprocess.Popen("gnome-terminal -- ./../Micro-XRCE-DDS-Agent/build/MicroXRCEAgent udp4 -p 8888",
                               stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
 
-for i in range(args.n_iter) :
-    
-    start = subprocess.Popen("docker start px4_ros2",stdout=subprocess.PIPE,universal_newlines=True, shell=True)
+for j in range (1,9):
+    for i in range(args.n_iter) :
+        
+        start = subprocess.Popen("docker start px4_ros2",stdout=subprocess.PIPE,universal_newlines=True, shell=True)
 
-    gazebo = subprocess.Popen("gnome-terminal -- docker exec -it px4_ros2 bash -c 'cd PX4-Autopilot; HEADLESS=1 make px4_sitl gazebo-classic'",
-                              stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
-    time.sleep(gazebo_startup_time)
-    traj = subprocess.Popen("gnome-terminal -- docker exec -it px4_ros2 bash -c 'cd sensitivity_experiment; source install/setup.bash; ros2 run trajectory_publisher trajectory_publisher'",
-                            stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
+        gazebo = subprocess.Popen("gnome-terminal -- docker exec -u 0 -it px4_ros2 bash -c 'cd PX4-Autopilot; HEADLESS=1 make px4_sitl gazebo-classic_iris_delta_p"+str(j)+"'",
+                                stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
+        time.sleep(gazebo_startup_time)
+        traj = subprocess.Popen("gnome-terminal -- docker exec -u 0 -it px4_ros2 bash -c 'cd sensitivity_experiment; source install/setup.bash; ros2 run trajectory_publisher trajectory_publisher'",
+                                stdout=subprocess.PIPE, stdin=subprocess.PIPE, encoding='utf8', shell=True)
 
-    time.sleep(args.traj_length + takeoff_time + 10)
-    stop = subprocess.Popen("docker stop px4_ros2",stdout=subprocess.PIPE,universal_newlines=True, shell=True)
-    time.sleep(2)
+        time.sleep(args.traj_length + takeoff_time + 10)
+        stop = subprocess.Popen("docker stop px4_ros2",stdout=subprocess.PIPE,universal_newlines=True, shell=True)
+        time.sleep(2)
+
