352bc730cb9231b3be66b0c3f9ece3d5815f986c
asr <ali.srour@seaowlgroup.com>
2025-02-16 14:00:00 +0100

    name changing

--- create_px4_container.sh
+++ create_px4_container.sh
@@ -6,7 +6,7 @@ docker run -it --privileged \
 --env=LOCAL_USER_ID="$(id -u)" \
 --env "DISPLAY" \
 -v $(pwd)/../PX4-Autopilot/:/home/user/PX4-Autopilot/:rw \
--v $(pwd)/../sensitivity_experiment/:/home/user/sensitivity_experiment/:rw \
+-v $(pwd)/../Sensitivity_PX4_Experiments/:/home/user/Sensitivity_PX4_Experiments/:rw \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 --network host \
 --workdir="/home/user/" \
